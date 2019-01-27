b2bfc955ed99fff545e81ca739fd7d4d8d008896
Sabrina Herrero <sabrinaherrero123@gmail.com>
2019-01-27 10:28:29 -0600

    Added a preprocessor for image upload
    Not yet using the S3 bucket API, only receives data without crashing.

--- app.py
+++ app.py
@@ -35,6 +35,22 @@ class Image(db.Model):
 # Creates the tables in the specified database using the models 
 db.create_all()
 
+#------processing funtions------#
+
+# Image POST preprocessor
+def image_post_preprocessor(data=None, **kw):
+   """Accepts a single argument, `data`, which is the dictionary of
+   fields to set on the new instance of the model.
+   """
+   # make call to S3 bucket to post image
+
+   # store url in data and remove image from data since it is not stored in the DB
+   data["link"] = "TEST"
+   del data["image"]
+
+   pass
+
+
 #------API Endpoints------#
 
 CORS(app) 
@@ -42,7 +58,15 @@ manager = APIManager(app, flask_sqlalchemy_db=db)
 
 # Created the basic API calls for each model 
 manager.create_api(Date, methods=['GET','POST','DELETE', 'PUT'], url_prefix=None)
-manager.create_api(Image, methods=['GET','POST','DELETE', 'PUT'], url_prefix=None)
+manager.create_api(Image, 
+   # methods allowed for Images
+   methods=['GET','POST','DELETE', 'PUT'],
+   # A list of preprocessors for each method.
+   preprocessors={
+      'POST': [image_post_preprocessor]
+   }, 
+   url_prefix=None)
+
 
 #------run the app------#
 
